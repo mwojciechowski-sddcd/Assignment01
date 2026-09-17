14f89a7f98b2049f753cff95897b51f05c8b71bb
Martin Wojciechowski <mwojciechowski@student.sdccd.edu>
2026-09-17 16:13:43 +0000

    Added changes up to step 14 alert

---
+++ js/script.js
@@ -0,0 +1,136 @@
+// STEP 1
+
+// let someMonth
+// function theMonth()
+// let currentMonth
+// let summerMonth
+// let myLibraryFunction
+
+// STEP 2
+
+// numeric literal expression
+//24
+
+// string literal expression
+//"My name is Martin!"
+
+// boolean literal expression
+//true
+
+// null literal expression
+//null
+
+// STEP 3
+
+// variable expression
+//let age = 20;
+
+// arithmetic expression with variable
+//age + 5
+
+// comparison expression
+//age >= 18
+
+// String expression with variable
+//"Hello, " + name
+
+// logical expression
+//age >= 18 && age < 50
+
+// function call
+//Math.max(10, 20)
+
+// assignment expression
+//age = age + 1
+
+// STEP 4
+
+// let strFirstName;
+// let strLastName;
+// let strAddress;
+// let strCity;
+// let strState;
+// let strZipCode;
+// let intYourAge;
+// let strReferralSource;
+// let boolMayWeContactYou;
+
+// STEP 5
+
+// let strLastName = "Wojciechowski";
+// let intYourAge = 34;
+// let boolMayWeContactYou = false;
+
+// STEP 6
+
+//let result = 10 + "3";
+
+//console.log(result);
+
+//result shows 13 in console after 10 is coerced into int value then result is coerced into String
+
+// STEP 7
+
+//let result = "Martin " + true
+//console.log(result);
+
+// The coerced result is String "Martin true" as bool true is coerced into String
+
+// let result = false + 9;
+// console.log(result);
+
+//The coerced result is 9. Looks like it treats false as 0.
+
+// let result = true + 9;
+// console.log(result);
+
+//The coerced result is 10. Looks like it treats true as 1.
+
+
+// STEP 8
+
+//let someString = "Who once said, \"Only two things are infinite, the universe and human stupidity, and I'm not sure about the former.\""
+//console.log(someString);
+
+// I had to switch from single to double quotes and escape the double quote characters with \" to have the inner quote appear in the String
+
+// STEP 9
+
+// let dog = null;
+// console.log(dog);
+
+// let cat;
+// console.log(cat);
+
+// dog produces a null value since it was initialized with a null value while cat produces an undefined value in console because it was not assigned
+
+// STEP 10
+
+// console.log(typeof "Hello World");  // string
+// console.log(typeof 13);             // number
+// console.log(typeof true);           // boolean 
+// console.log(typeof {});             // object
+// console.log(typeof undefined);      // undefined
+
+// STEP 11
+
+//alert("Hello " + "Martin Wojciechowski" + ", welcome to the JavaScript class!");
+
+// STEP 12
+
+// let name = "Martin Wojciechowski";
+// alert("Hello " + name + ", welcome to the JavaScript class!");
+
+// STEP 13
+
+// let course = "JavaScript";
+// let name = "Martin Wojciechowski";
+// alert("Hello " + name + ", welcome to the " + course + " class!");
+
+// STEP 14
+
+alert(typeof "Hello"); 
+
+let course = "JavaScript";
+let name = "Martin Wojciechowski";
+alert("Hello " + name + ", welcome to the " + course + " class!");
